49666f3111adb00620de21ecf2048c68898c45c8
FKLSG <karleong79@gmail.com>
2016-07-03 08:09:17 +0800

    Trial

--- Helloworld.py
+++ Helloworld.py
@@ -1,5 +1,24 @@
-print "Hello, World!"
-print "Goodbye, World!"
-print "FKL"
-print "FKL New"
+x=10
+if (x>9):
+    print ("big number")
+else:
+    print ("small number")
+y = range (5,10,1)
+print y
+
+
+def func(x):
+    x = x+1
+    print "X = ", x
+
+func(3)
+func(4)
+
+def make_incrementor(n):
+    return lambda x: x +n
+
+f = make_incrementor(40)
+print f(2)
+
+
 
